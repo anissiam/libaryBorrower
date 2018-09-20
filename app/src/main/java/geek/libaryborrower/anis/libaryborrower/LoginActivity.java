4f368931f1b929f08037d92194c5946377c7798a
anissiam <anis.siam@hotmail.com>
2018-09-20 22:18:40 +0300

    //login screen completed sign up ready to design

--- app/src/main/java/geek/libaryborrower/anis/libaryborrower/LoginActivity.java
+++ app/src/main/java/geek/libaryborrower/anis/libaryborrower/LoginActivity.java
@@ -1,13 +1,84 @@
 package geek.libaryborrower.anis.libaryborrower;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.iid.FirebaseInstanceId;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText edtEmail;
+    private EditText edtPassword;
+    private TextView tvForgetPassword;
+    private RelativeLayout relative_google;
+    private Button btnLogin;
+    private TextView tvSignUp;
+    private ProgressBar progressBar;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        edtEmail = (EditText) findViewById(R.id.edtEmail);
+        edtPassword = (EditText) findViewById(R.id.edtPassword);
+        tvForgetPassword = (TextView) findViewById(R.id.txForgetPassword);
+        relative_google = (RelativeLayout) findViewById(R.id.relative_google);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        tvSignUp = (TextView) findViewById(R.id.tvSignUp);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        tvSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signUpIntent = new Intent(LoginActivity.this,SignupActivity.class);
+                startActivity(signUpIntent);
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = edtEmail.getText().toString().trim();
+                String password = edtPassword.getText().toString();
+
+                if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
+                    progressBar.setVisibility(View.VISIBLE);
+                    mAuth.signInWithEmailAndPassword(email, password)
+                            .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+
+                            Toast.makeText(LoginActivity.this, "success ", Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    }
+            }
+        });
     }
 }
+
+
+
+
